21bb0dfa07c372674c3f5e90488c590537ccb9ab
Jason Natividad <jainatividad@yahoo.com>
2017-05-02 22:04:45 -0700

    Start thumbnail section on right side of video player

---
+++ ops-tube/js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jasonnatividad on 5/2/17.
+ */
+
+
+//
+//<video width="470" height="255" poster="placeholder.png" controls>
+//<source src="video.mp4" type="video/mp4">
+//    <source src="video.ogg" type="video/ogg">
+//    <source src="video.webm" type="video/webm">
+//    <object data="video.mp4" width="470" height="255">
+//    <embed src="video.swf" width="470" height="255">
+//    </object>
+//    </video>
